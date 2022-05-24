52ad47bb20b8c9017af01da18b824fde9944492c
Wesley Pivetta <wesley@ephealth.onmicrosoft.com>
2022-05-23 21:22:57 -0300

    fix(): ajustando evaluate do getIpa para pegar a table

--- src/service/scrapper-service.ts
+++ src/service/scrapper-service.ts
@@ -1,17 +1,21 @@
 import puppeteer from 'puppeteer';
+import { ids } from '../util/beerSelectorIdList';
 
 const getIPAs = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://mybest-brazil.com.br/19203');
-
-    const ipas = await page.$eval('#\31 816542 > div > table', (el) => {
-        console.log({el})
-        return (<any>el).value;
+    const ipa = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll("#\\31 816542 > div > table")).map(el => el.textContent);
     })
+    console.log(ipa)
+    // for (let index = 0; index < ids.length; index++) {
+    //     const ipa = await page.evaluate(() => {
+    //         return Array.from(document.querySelectorAll(`#\\${ids[index]} > div > table`)).map(el => el.textContent);
+    //     })
+    //     console.log(ipa)
+    // }
     await page.screenshot({path: './screenshots/ipas.png'});
-
-
 }
 
 export { getIPAs };
