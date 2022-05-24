e84f38cf325786c94288ad8e6ee0ac2c474107bb
Wesley Pivetta <wesley@ephealth.onmicrosoft.com>
2022-05-23 21:46:18 -0300

    fix(): improving getIPAs function, now it gets all the 10 beers table's

--- src/service/scrapper-service.ts
+++ src/service/scrapper-service.ts
@@ -5,10 +5,11 @@ const getIPAs = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://mybest-brazil.com.br/19203');
-    const ipa = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll("#\\31 816542 > div > table")).map(el => el.textContent);
-    })
-    console.log(ipa)
+    const ipa = await page.$$eval("div > div > table", elements => elements.map(
+        item => item.textContent
+    ));
+    ipa.pop();
+    await console.log(ipa)
     await page.screenshot({path: './screenshots/ipas.png'});
 }
 
