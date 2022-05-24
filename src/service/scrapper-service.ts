0fe67cb2bde10e07117155786e574c75856ed0f6
Wesley Pivetta <wesley@ephealth.onmicrosoft.com>
2022-05-23 21:50:10 -0300

    fix(): removed unused file

--- src/service/scrapper-service.ts
+++ src/service/scrapper-service.ts
@@ -1,15 +1,14 @@
 import puppeteer from 'puppeteer';
-import { ids } from '../util/beerSelectorIdList';
 
 const getIPAs = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://mybest-brazil.com.br/19203');
-    const ipa = await page.$$eval("div > div > table", elements => elements.map(
+    const ipas = await page.$$eval("div > div > table", elements => elements.map(
         item => item.textContent
     ));
-    ipa.pop();
-    await console.log(ipa)
+    ipas.pop();
+    await console.log(ipas)
     await page.screenshot({path: './screenshots/ipas.png'});
 }
 
