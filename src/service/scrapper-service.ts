c23c8600ee8e4bfef4af2637abce5b3d3edecdf8
Wesley Pivetta <wesley@ephealth.onmicrosoft.com>
2022-05-23 21:31:14 -0300

    fix(): removing commented garbage code

--- src/service/scrapper-service.ts
+++ src/service/scrapper-service.ts
@@ -9,12 +9,6 @@ const getIPAs = async () => {
         return Array.from(document.querySelectorAll("#\\31 816542 > div > table")).map(el => el.textContent);
     })
     console.log(ipa)
-    // for (let index = 0; index < ids.length; index++) {
-    //     const ipa = await page.evaluate(() => {
-    //         return Array.from(document.querySelectorAll(`#\\${ids[index]} > div > table`)).map(el => el.textContent);
-    //     })
-    //     console.log(ipa)
-    // }
     await page.screenshot({path: './screenshots/ipas.png'});
 }
 
