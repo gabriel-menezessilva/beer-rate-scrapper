2d117908c9a3a44e4dc0df6c512ae216067323f6
Wesley Pivetta <wesley@ephealth.onmicrosoft.com>
2022-05-30 20:57:44 -0300

    feat(): adicionando tabela de cervejas alemas

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer';
+import { getGermanBeer } from './src/service/german-beer-service';
 import { getIPAs } from './src/service/ipa-service';
 import { getPilsen } from './src/service/pilsen-service';
 import { getSingleMalt } from './src/service/single-malt-service';
@@ -8,10 +9,11 @@ import { db } from './src/util/admin';
 (async () => {
 
   try {
-    getIPAs();
-    getPilsen();
-    getStout();
-    getSingleMalt();
+    // getIPAs();
+    // getPilsen();
+    // getStout();
+    // getSingleMalt();
+    getGermanBeer();
   } catch (e) {
     console.log(e);
   }
---
+++ src/service/german-beer-service.ts
@@ -0,0 +1,32 @@
+import puppeteer from 'puppeteer';
+
+const getGermanBeer = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://mybest-brazil.com.br/19286');
+
+    const content = await page.$$eval("div > div > table tbody", bodies => {
+        return bodies.map(body => {
+            const rows = body.querySelectorAll('tr');
+
+            return Array.from(rows, row => row.querySelector('td')?.innerText);
+        })
+    });
+
+    const names = await page.$$eval(".c-panel__right > h3", headers => {
+        return headers.map(h => {
+            const beerNames = h.querySelectorAll('.c-panel__heading');
+            return Array.from(beerNames, beer => beer.textContent);
+        }).flat();
+    })
+
+    names.shift();
+
+    const germanBeer = content.slice(0, 10)
+                        .map((c, i) => ({name: names[i], style: c[0], ibu: c[2], abc: c[3]}));
+
+    console.log(germanBeer)
+    await page.screenshot({path: './screenshots/german-beer.png'});
+}
+
+export { getGermanBeer };
