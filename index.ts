754908fbf2af7177a7a89b113b9034b3b348487d
Wesley Pivetta <wesley@ephealth.onmicrosoft.com>
2022-05-30 19:44:57 -0300

    feat

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import puppeteer from 'puppeteer';
-import { getIPAs } from './src/service/scrapper-service';
+import { getIPAs } from './src/service/ipa-service';
+import { getPilsen } from './src/service/pilsen-service';
 import { db } from './src/util/admin';
 
 (async () => {
 
   try {
     getIPAs();
+    getPilsen();
   } catch (e) {
     console.log(e);
   }
---
+++ src/service/pilsen-service.ts
@@ -0,0 +1,32 @@
+import puppeteer from 'puppeteer';
+//TODO: TERMINAR DE MONTAR A TABELA, SÓ TÔ SUBINDO A ALTERAÇÃO DOS ARQUIVOS
+const getPilsen = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://mybest-brazil.com.br/19217');
+
+    const content = await page.$$eval("div > div > table tbody", bodies => {
+        return bodies.map(body => {
+            const rows = body.querySelectorAll('tr');
+
+            return Array.from(rows, row => row.querySelector('td')?.innerText);
+        })
+    });
+
+    const names = await page.$$eval(".c-panel__right > h3", headers => {
+        return headers.map(h => {
+            const beerNames = h.querySelectorAll('.c-panel__heading');
+            return Array.from(beerNames, beer => beer.textContent);
+        }).flat();
+    })
+
+    names.shift();
+
+    const pilsen = content.slice(0, 10)
+                        .map((c, i) => ({name: names[i], style: c[0], color: c[1], dryHopping: c[2], ibu: c[3], abc: c[4]}));
+
+    console.log(pilsen)
+    await page.screenshot({path: './screenshots/ipas.png'});
+}
+
+export { getPilsen };
